d88fa4d42e68f0d8f66a4d86b87d1eeffffd0de9
Björn Larsson <b.e.larsson@gmail.com>
2021-01-30 15:29:28 +0100

    Add package file

---
+++ homebrew-manager.el
@@ -0,0 +1,25 @@
+;;; homebrew-manager.el --- Manage your homebrew packages through Emacs -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021 Björn Larsson
+;;
+;; Author: Björn Larsson <http://github/fuzzycode>
+;; Maintainer: Björn Larsson <develop@bjornlarsson.net>
+;; Created: January 30, 2021
+;; Modified: January 30, 2021
+;; Version: 0.0.1
+;; Keywords:
+;; Homepage: https://github.com/fuzzycode/homebrew-manager
+;; Package-Requires: ((emacs "24.3") (s "1.11.0") (dash "2.12.0"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;; Code:
+
+
+
+(provide 'homebrew-manager)
+;;; homebrew-manager.el ends here
