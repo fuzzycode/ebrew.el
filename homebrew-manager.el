d410ee0628d4392c713b01296a8dd2d161d5fb7a
Björn Larsson <b.e.larsson@gmail.com>
2021-02-09 19:54:04 +0100

    Make functionality asynchronous

--- homebrew-manager.el
+++ homebrew-manager.el
@@ -57,32 +57,89 @@
   :group 'homebrew-manager)
 
 
+(defcustom hbm-outdated-version-face "orange"
+  "Face to use when rendering version of outdated packages."
+  :type'face
+  :group 'homebrew-manager)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Internals
+(cl-defstruct (homebrew-package (:constructor homebrew-package-create)
+                                (:copier nil))
+  "Store information about an installed homebrew package"
+  name version leaf? pinned? latest type)
 
-(defun homebrew--call (cmd &rest arguments)
-  "Call brew CMD with provided ARGUMENTS."
-  (let ((command (format "brew %s %s" cmd (s-join " " arguments))))
-    (s-split "\n" (shell-command-to-string command) t)))
+(defun homebrew--package-render (package)
+  "Render the PACKAGE struct in a tabulated-list friendly format."
+  (list package (vector (homebrew-package-name package)
+                        (homebrew-package-version package)
+                        (if (homebrew-package-latest package) (propertize (homebrew-package-latest package) 'font-lock-face `(:foreground ,hbm-outdated-version-face)) "")
+                        (if (homebrew-package-leaf? package) "*" "")
+                        (if (homebrew-package-pinned? package) "*" ""))))
 
-(defun homebrew--list-packages ()
-  "List all packages installed through brew."
-  (let ((leaves (homebrew--call "leaves"))
-        (outdated (homebrew--call "outdated"))
-        (pinned (homebrew--call "pinned")))
-    (cl-labels ((filter (item)
-                        (let* ((items (split-string item))
-                               (name (car items)))
-                          (list name (vconcat (-concat items
-                                                       (if (-contains? leaves name) '("*") '(""))
-                                                       (if (-contains? outdated name) '("*") '(""))
-                                                       (if (-contains? pinned name) '("*") '(""))))))))
-      (-map #'filter (homebrew--call "list" "--version")))))
+(defun homebrew--parse-outdated (outdated)
+  "Parse the list of OUTDATED packages into an alist of name version pairs."
+  (-map (lambda (item)
+          (let* ((split (s-split "<" item))
+                 (latest (nth 1 split))
+                 (name (car (s-split " " (nth 0 split)))))
+            `(,name . ,latest))) outdated))
 
-(defun homebrew-package--apply-packages (action packages)
-  "Call brew ACTION with the list of PACKAGES as argument."
-  (shell-command-to-string (format "brew %s %s" action (s-join " " packages))))
+(defun homebrew--get-latest-version (name outdated)
+  "Parse OUTDATED searching for NAME, returning the latest known version of NAME or nil if not found."
+  (cdr (assoc-string name outdated)))
 
+(defun brew--update-package-list ()
+  "Update the list of packages async."
+  (deferred:$
+    (deferred:parallel
+      (lambda () (deferred:process-shell "brew" "leaves"))
+      (lambda () (deferred:process-shell "brew" "list" "--formula" "--versions"))
+      (lambda () (deferred:process-shell "brew" "list" "--pinned"))
+      (lambda () (deferred:process-shell "brew" "outdated" "--formula")))
+    (deferred:nextc it
+      (lambda (result)
+        (let ((leaves (s-split "\n" (nth 0 result) t))
+              (formula (s-split "\n" (nth 1 result) t))
+              (pinned (s-split "\n" (nth 2 result) t))
+              (outdated (homebrew--parse-outdated (s-split "\n" (nth 3 result)))))
+          (cl-loop for f in formula
+                   for name = (nth 0 (s-split " " f))
+                   for version = (nth 1 (s-split " " f))
+                   collect (homebrew-package-create :name name
+                                                    :type 'formula
+                                                    :version version
+                                                    :latest (homebrew--get-latest-version name outdated)
+                                                    :pinned? (-contains? pinned name)
+                                                    :leaf? (-contains? leaves name))))))
+    (deferred:nextc it
+      (lambda (packages)
+        (let ((buffer (get-buffer-create "*Brew installed packages*")))
+          (with-current-buffer buffer
+            (switch-to-buffer buffer)
+            (brew-package-mode)
+            (setq tabulated-list-entries (-map #'homebrew--package-render packages))
+            (tabulated-list-print))))))
+  t)
+
+(defun homebrew--apply-changes (update delete pin unpin)
+  "Apply the changes to all tagged packages."
+  (deferred:$
+    (deferred:next
+      (lambda () (when pin (deferred:process-shell "brew" "pin" (s-join " " (-map #'homebrew-package-name pin))))))
+    (deferred:nextc it
+      (lambda (_) (when unpin (deferred:process-shell "brew" "unpin" (s-join " " (-map #'homebrew-package-name unpin))))))
+    (deferred:nextc it
+      (lambda (_) (when update (deferred:process-shell "brew" "upgrade" (s-join " " (-map #'homebrew-package-name update))))))
+    (deferred:nextc it
+      (lambda (_) (when delete (deferred:process-shell "brew" "delete" (s-join " " (-map #'homebrew-package-name delete))))))
+    (deferred:nextc it
+      (lambda (_) (brew--update-package-list))))
+  t)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Mode
 
 (defvar brew-package-mode-map
   (let ((map (make-sparse-keymap)))
@@ -107,31 +164,12 @@
   (setq truncate-lines t)
   (setq tabulated-list-format `[("Name" ,hbm-name-column-width t)
                                ("Version" ,hbm-version-column-width nil)
+                               ("Latest" ,hbm-version-column-width nil)
                                ("Leaf" 4 nil)
-                               ("Outdated" 8 nil)
-                               ("Pinned" 0 nil)])
-
-  (setq tabulated-list-entries #'homebrew--list-packages)
+                               ("Pinned" 6 nil)])
 
   (setq tabulated-list-padding 2)
-
-  (tabulated-list-init-header)
-  (tabulated-list-print))
-
-;;;;;;;;;;;;;;;;;;
-;; Predicates
-
-(defun homebrew-package-pinned-p (item)
-  "Check if ITEM is pinned or not."
-  (s-present? (aref item 4)))
-
-(defun homebrew-package-outdated-p (item)
-  "Check if ITEM is outdated or not."
-  (s-present? (aref item 3)))
-
-(defun homebrew-package-leaf-p (item)
-  "Check if ITEM is a leaf or not."
-  (s-present? (aref item 2)))
+  (tabulated-list-init-header))
 
 ;;;;;;;;;;;;;;;;;;
 ;; Interaction
@@ -144,25 +182,25 @@
 (defun homebrew-package-pin ()
   "Mark item at point for pinning."
   (interactive)
-  (unless (homebrew-package-pinned-p (tabulated-list-get-entry) )
+  (unless (homebrew-package-pinned? (tabulated-list-get-id) )
     (tabulated-list-put-tag "p" t)))
 
 (defun homebrew-package-unpin ()
   "Mark item at point for un-pinning."
   (interactive)
-  (when (homebrew-package-pinned-p (tabulated-list-get-entry))
+  (when (homebrew-package-pinned? (tabulated-list-get-id))
     (tabulated-list-put-tag "P" t)))
 
 (defun homebrew-package-update ()
   "Mark item at point for updating."
   (interactive)
-  (when (homebrew-package-outdated-p (tabulated-list-get-entry))
+  (when (homebrew-package-latest (tabulated-list-get-id))
     (tabulated-list-put-tag "U" t)))
 
 (defun homebrew-package-delete ()
   "Mark item at point for deletion."
   (interactive)
-  (when (homebrew-package-leaf-p (tabulated-list-get-entry))
+  (when (homebrew-package-leaf? (tabulated-list-get-id))
     (tabulated-list-put-tag "D" t)))
 
 (defun homebrew-package-upgrade-all ()
@@ -177,33 +215,24 @@
   (let (update-list delete-list pinned-list unpinned-list tag id)
     (save-excursion
       (goto-char (point-min))
-        (while (not (eobp)) ;; scan the buffer for tagged items
-          (setq tag (char-after))
-          (setq id (tabulated-list-get-id))
-          (cond ((eq tag ?D) (push id delete-list))
-                ((eq tag ?U) (push id update-list))
-                ((eq tag ?p) (push id pinned-list))
-                ((eq tag ?P) (push id unpinned-list)))
-          (forward-line))
-        ;; Sanity check
-        (unless (or update-list delete-list pinned-list unpinned-list)
-          (user-error "No package marked for action"))
-
-        (when pinned-list
-          (homebrew-package--apply-packages "pin" pinned-list))
-        (when unpinned-list
-          (homebrew-package--apply-packages "unpin" unpinned-list))
-        (when update-list
-          (homebrew-package--apply-packages "upgrade" update-list))
-        (when delete-list
-          (homebrew-package--apply-packages "uninstall" delete-list))))
-  (tabulated-list-print))
+      (while (not (eobp)) ;; scan the buffer for tagged items
+        (setq tag (char-after))
+        (setq id (tabulated-list-get-id))
+        (cond ((eq tag ?D) (push id delete-list))
+              ((eq tag ?U) (push id update-list))
+              ((eq tag ?p) (push id pinned-list))
+              ((eq tag ?P) (push id unpinned-list)))
+        (forward-line))
+      ;; Sanity check
+      (unless (or update-list delete-list pinned-list unpinned-list)
+        (user-error "No package marked for action"))
+      (homebrew--apply-changes update-list delete-list pinned-list unpinned-list))))
 
 ;;;###autoload
 (defun homebrew-package-info (&optional package)
   "Show the information for PACKAGE or the current package at point."
   (interactive)
-  (let ((pkg (or package (tabulated-list-get-id))))
+  (let ((pkg (or package (homebrew-package-name (tabulated-list-get-id)))))
     (deferred:$
       (deferred:process-shell "brew" "info" "--formula" pkg)
       (deferred:nextc it
@@ -214,7 +243,10 @@
               (goto-char (point-min))
               (help-mode)
               (goto-address-mode))
-            (display-buffer buffer))))))
+            (display-buffer buffer))))
+      (deferred:error it
+        (lambda (_)
+          (message "No such package found!: %s" package)))))
   package)
 
 ;;;###autoload
@@ -225,17 +257,16 @@
     (deferred:process-shell "brew" "update")
     (deferred:nextc it
       (lambda (_)
-        (tabulated-list-print)
-        (message "Homebrew package list updated")))))
+        (brew--update-package-list)))))
 
 ;;;###autoload
 (defun homebrew-list-packages ()
   "Lists all homebrew installed packages in a tabulated list."
   (interactive)
-  (let ((buffer (generate-new-buffer "*brew user list*")))
-    (with-current-buffer buffer
-      (brew-package-mode))
-    (display-buffer buffer)))
+  (message "Fetching list of installed packages")
+  (display-buffer (generate-new-buffer "*Brew installed packages*"))
+  (brew--update-package-list))
+
 
 (provide 'homebrew-manager)
 ;;; homebrew-manager.el ends here
